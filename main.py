3709c22dd414b953e7e7045e9fc3d9604dfbdf69
Stephen Bradshaw <stephen.mark.bradshaw@gmail.com>
2025-08-06 12:22:03 +1000

    Updates

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 from flask import Flask, request, Response
 import requests
 import os
+import urllib3
 
 # allows for debugging
 DEBUG=os.getenv('DEBUG', False) 
+SCHEME=os.getenv('SCHEME','http')
+TESTROUTE=os.getenv('TESTROUTE', 'b8af860c6c7f78d5cbcaa86c8f11b268cd0c0295') 
+
 
 app = Flask(__name__)
 
 
-# change this url to remove indicator
-@app.route('/b8af860c6c7f78d5cbcaa86c8f11b268cd0c0295')
+@app.route(f'/{TESTROUTE}')
 def health():
     return "OK"
 
@@ -21,12 +24,16 @@ def root(url):
     timeout = int(os.getenv('TIMEOUT', 20)) # override using TIMEOUT env variable
     path = f'http://{host}/{url}'
     try:
+        headers = dict(request.headers)
+        if not 'accept-encoding' in [a.lower() for a in headers.keys()]:
+            headers['Accept-Encoding'] = urllib3.util.SKIP_HEADER # stop requests from adding this header
         r = requests.request(request.method, path, params=request.args, stream=True, 
-                            headers=dict(request.headers), allow_redirects=False, 
+                            headers=headers, allow_redirects=False, 
                             data=request.get_data(), timeout=timeout)
         def generate():
             for chunk in r.raw.stream(decode_content=False):
                 yield chunk
+
         out = Response(generate(), headers=dict(r.raw.headers))
         out.status_code = r.status_code
         return out
@@ -37,7 +44,6 @@ def root(url):
         else:
             return 'Error'
     
-
 
 
 if __name__ == "__main__":
